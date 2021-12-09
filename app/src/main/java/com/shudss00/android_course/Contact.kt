c5dd94fa5b6a1958cd030bab3d4f97102cb40a15
Semyon <shudss00@gmail.com>
2021-12-09 15:00:08 +0400

    feat: change function from ContactService.kt: getContactList and getContactById, change POJO class Contact.kt, override function onRequestPermissionResult in MainActivity.kt

--- app/src/main/java/com/shudss00/android_course/Contact.kt
+++ app/src/main/java/com/shudss00/android_course/Contact.kt
@@ -1,84 +1,16 @@
 package com.shudss00.android_course
 
-import java.util.Calendar
-import java.util.GregorianCalendar
+import android.net.Uri
+import java.util.*
 
 data class Contact(
-        val name: String,
-        val phoneNumber: String,
-        val extraPhoneNumber: String,
-        val email: String,
-        val extraEmail: String,
-        val img: String,
-        val description: String,
-        val dayOfBirth: Calendar
-    ) {
-    val id: Int = countOfContacts++
-
-    companion object {
-        var countOfContacts = 0
-    }
-}
-
-val contactsList = listOf(
-    Contact(
-        name = "Elon Musk",
-        phoneNumber = "+7-958-786-9849",
-        extraPhoneNumber = "+7-922-773-6243",
-        email = "musk0@mail.com",
-        extraEmail = "musk1@mail.com",
-        img = "https://goo.su/8dk1",
-        description = "Genius",
-        dayOfBirth = GregorianCalendar(1971, Calendar.JULY, 28)
-    ),
-    Contact(
-        name = "Anonymous",
-        phoneNumber = "+7-973-883-3184",
-        extraPhoneNumber = "+7-800-151-8709",
-        email = "nymus@mail.com",
-        extraEmail = "anony@mail.com",
-        img = "https://goo.su/8DJZ",
-        description = "yoli pali",
-        dayOfBirth = GregorianCalendar(1999, Calendar.NOVEMBER, 20)
-    ),
-    Contact(
-        name = "Android",
-        phoneNumber = "+7-998-503-6145",
-        extraPhoneNumber = "+7-922-297-7902",
-        email = "andrey7@mail.com",
-        extraEmail = "droid@mail.com",
-        img = "https://goo.su/8dk6",
-        description = "Boss",
-        dayOfBirth = GregorianCalendar(2008, Calendar.SEPTEMBER, 23)
-    ),
-    Contact(
-        name = "Homer Simpson",
-        phoneNumber = "+7-943-773-3456",
-        extraPhoneNumber = "+7-922-369-3443",
-        email = "homer@mail.com",
-        extraEmail = "simpson@mail.com",
-        img = "https://goo.su/8dKa",
-        description = "I want to eat pelmeny",
-        dayOfBirth = GregorianCalendar(1971, Calendar.JULY, 28)
-    ),
-    Contact(
-        name = "God",
-        phoneNumber = "+7-000-000-0001",
-        extraPhoneNumber = "+7-922-030-7956",
-        email = "iisus@paradise.com",
-        extraEmail = "hristos@mail.com",
-        img = "https://goo.su/8dkC",
-        description = "Creator",
-        dayOfBirth = GregorianCalendar(1956, Calendar.MAY, 12)
-    ),
-    Contact(
-        name = "Papich",
-        phoneNumber = "+7-740-040-8764",
-        extraPhoneNumber = "+7-564-362-9846",
-        email = "vitaly@mail.com",
-        extraEmail = "cal@mail.com",
-        img = "https://goo.su/8dSp",
-        description = "Ya ne ponimay",
-        dayOfBirth = GregorianCalendar(1990, Calendar.NOVEMBER, 19)
-    )
+    var id: Int = 0,
+    var name: String = "",
+    var phoneNumber: String = "",
+    var extraPhoneNumber: String = "",
+    var email: String = "",
+    var extraEmail: String = "",
+    var img: Uri = Uri.EMPTY,
+    var description: String = "",
+    var dayOfBirth: Calendar? = null
 )
--- app/src/main/java/com/shudss00/android_course/ContactService.kt
+++ app/src/main/java/com/shudss00/android_course/ContactService.kt
@@ -2,30 +2,21 @@ package com.shudss00.android_course
 
 import android.app.Service
 import android.content.Intent
+import android.net.Uri
 import android.os.Binder
 import android.os.IBinder
+import android.provider.ContactsContract
 import java.lang.ref.WeakReference
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.*
+import java.text.ParseException
+import java.text.SimpleDateFormat
+
 
 class ContactService : Service() {
     private var binder: Binder? = ContactBinder()
     private var executor: ExecutorService? = Executors.newFixedThreadPool(1)
-
-    fun getContactList(resultListener: ContactListFragment.ContactsLoadListener) {
-        val ref = WeakReference(resultListener)
-        executor?.execute { ref.get()?.onLoaded(contactsList) }
-    }
-
-    fun getContactById(
-        contactId: Int?,
-        resultListener: ContactDetailsFragment.ContactLoadListener
-    ) {
-        val ref = WeakReference(resultListener)
-        executor?.execute {
-            ref.get()?.onLoaded(contactsList.find { it.id == contactId })
-        }
-    }
 
     inner class ContactBinder : Binder() {
         fun getService(): ContactService = this@ContactService
@@ -39,5 +30,225 @@ class ContactService : Service() {
         binder = null
         executor = null
         return super.onUnbind(intent)
+    }
+
+    fun getContactList(resultListener: ContactListFragment.ContactsLoadListener) {
+        val ref = WeakReference(resultListener)
+        executor?.execute {
+            val contactList = mutableListOf<Contact>()
+            val cursor = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                arrayOf(
+                    ContactsContract.Contacts._ID,
+                    ContactsContract.Contacts.PHOTO_URI,
+                    ContactsContract.Contacts.DISPLAY_NAME,
+                    ContactsContract.Contacts.HAS_PHONE_NUMBER
+                ),
+                null,
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+            )
+            cursor.use {
+                if (cursor != null) {
+                    val idColumnIndex = cursor.getColumnIndex(
+                        ContactsContract.Contacts._ID
+                    )
+                    val nameColumnIndex = cursor.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME
+                    )
+                    val imageUriColumnIndex = cursor.getColumnIndex(
+                        ContactsContract.Contacts.PHOTO_URI
+                    )
+                    val hasPhoneNumberColumnIndex = cursor.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER
+                    )
+                    var imageString: String?
+                    var numberList: List<String>?
+                    var hasNumbers: Int?
+                    var contact: Contact?
+                    cursor.moveToFirst()
+                    while (!cursor.isAfterLast) {
+                        contact = Contact()
+                        contact.id = cursor.getInt(idColumnIndex)
+                        contact.name = cursor.getString(nameColumnIndex)
+
+                        imageString = cursor.getString(imageUriColumnIndex)
+                        if (imageString != null) {
+                            contact.img = Uri.parse(imageString)
+                        }
+
+                        hasNumbers = cursor.getInt(hasPhoneNumberColumnIndex)
+                        if (hasNumbers == 1) {
+                            numberList = getContactPhoneNumbersFromContentProvider(contact.id)
+                            if (numberList.isNotEmpty()) {
+                                contact.phoneNumber = numberList[0]
+                            }
+                        }
+
+                        contactList.add(contact)
+                        cursor.moveToNext()
+                    }
+                }
+            }
+            ref.get()?.onLoaded(contactList)
+        }
+    }
+
+    fun getContactById(
+        contactId: Int,
+        resultListener: ContactDetailsFragment.ContactLoadListener
+    ) {
+        val ref = WeakReference(resultListener)
+        executor?.execute {
+            val contact = Contact()
+            val contactCursor = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                arrayOf(
+                    ContactsContract.Contacts._ID,
+                    ContactsContract.Contacts.PHOTO_URI,
+                    ContactsContract.Contacts.DISPLAY_NAME,
+                    ContactsContract.Contacts.HAS_PHONE_NUMBER
+                ),
+                ContactsContract.Contacts._ID + " = ?",
+                arrayOf(contactId.toString()),
+                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+            )
+            contactCursor.use {
+                if (contactCursor != null) {
+                    val nameColumnIndex = contactCursor.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME
+                    )
+                    val imageUriColumnIndex = contactCursor.getColumnIndex(
+                        ContactsContract.Contacts.PHOTO_URI
+                    )
+                    val hasPhoneNumberColumnIndex = contactCursor.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER
+                    )
+                    var imageString: String?
+                    var numberList: List<String>?
+                    var emailList: List<String>?
+                    var hasNumbers: Int?
+                    contactCursor.moveToFirst()
+                    while (!contactCursor.isAfterLast) {
+                        contact.name = contactCursor.getString(nameColumnIndex)
+
+                        imageString = contactCursor.getString(imageUriColumnIndex)
+                        if (imageString != null) {
+                            contact.img = Uri.parse(imageString)
+                        }
+
+                        contact.dayOfBirth = getContactBirthdayFromContentProvider(contactId)
+
+                        hasNumbers = contactCursor.getInt(hasPhoneNumberColumnIndex)
+                        if (hasNumbers == 1) {
+                            numberList = getContactPhoneNumbersFromContentProvider(contactId)
+                            if (numberList.isNotEmpty()) {
+                                contact.phoneNumber = numberList[0]
+                            }
+                            if (numberList.size > 1) {
+                                contact.extraPhoneNumber = numberList[1]
+                            }
+                        }
+
+                        emailList = getContactEmailsFromContentProvider(contactId)
+                        if (emailList.isNotEmpty()) {
+                            contact.email = emailList[0]
+                        }
+                        if (emailList.size > 1) {
+                            contact.extraEmail = emailList[1]
+                        }
+
+                        contactCursor.moveToNext()
+                    }
+                }
+            }
+            ref.get()?.onLoaded(contact)
+        }
+    }
+
+    private fun getContactBirthdayFromContentProvider(contactId: Int?): Calendar? {
+        var birthday: Calendar? = null
+        val cursorBirthday = contentResolver.query(
+            ContactsContract.Data.CONTENT_URI,
+            arrayOf(ContactsContract.CommonDataKinds.Event.START_DATE),
+            ContactsContract.Data.CONTACT_ID
+                    + " = ? AND "
+                    + ContactsContract.CommonDataKinds.Event.TYPE
+                    + " = "
+                    + ContactsContract.CommonDataKinds.Event.TYPE_BIRTHDAY,
+            arrayOf(contactId.toString()),
+            null
+        )
+        try {
+            if (cursorBirthday != null && cursorBirthday.count > 0) {
+                cursorBirthday.moveToFirst()
+                val date = cursorBirthday.getString(
+                    cursorBirthday.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Event.START_DATE
+                    )
+                )
+                val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val calendar: Calendar = GregorianCalendar()
+                try {
+                    calendar.time = format.parse(date)
+                } catch (e: ParseException) {
+                    calendar.timeInMillis = 0
+                }
+                birthday = calendar
+            }
+        } finally {
+            cursorBirthday?.close()
+        }
+        return birthday
+    }
+
+    private fun getContactEmailsFromContentProvider(contactId: Int?): List<String> {
+        val emailList = mutableListOf<String>()
+        val cursorEmail = contentResolver.query(
+            ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+            arrayOf(ContactsContract.CommonDataKinds.Email.ADDRESS),
+            ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+            arrayOf(contactId.toString()),
+            null
+        )
+        try {
+            if (cursorEmail != null) {
+                val address: Int =
+                    cursorEmail.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS)
+                cursorEmail.moveToFirst()
+                while (!cursorEmail.isAfterLast) {
+                    emailList.add(cursorEmail.getString(address))
+                    cursorEmail.moveToNext()
+                }
+            }
+        } finally {
+            cursorEmail?.close()
+        }
+        return emailList
+    }
+
+    private fun getContactPhoneNumbersFromContentProvider(contactId: Int?): List<String> {
+        val numberList = mutableListOf<String>()
+        val phoneCursor = contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER),
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+            arrayOf(contactId.toString()),
+            null
+        )
+        try {
+            if (phoneCursor != null) {
+                val number =
+                    phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                phoneCursor.moveToFirst()
+                while (!phoneCursor.isAfterLast) {
+                    numberList.add(phoneCursor.getString(number))
+                    phoneCursor.moveToNext()
+                }
+            }
+        } finally {
+            phoneCursor?.close()
+        }
+        return numberList
     }
 }
